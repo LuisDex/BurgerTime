7c2fe1fb62630d96a472d755f699e768623971e0
Xerdet <lurobper@gmail.com>
2019-03-08 21:59:58 -0600

    Initial Upload to Heroku

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "YOURHOSTNAMEHERE",
+  host: "localhost",
   port: 3306,
-  user: "YOURUSERNAMEHERE",
-  password: "YOURPASSWORDHERE",
+  user: "root",
+  password: "root",
   database: "burgers_db"
 });
 
